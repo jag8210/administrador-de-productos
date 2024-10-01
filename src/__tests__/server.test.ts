01f14381001189bd6b32641271e3059a98e922e7
Andres Giraldo <jag8210@gmail.com>
2024-10-01 07:40:06 -0500

    Documentacion de la API agregada

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,12 +1,19 @@
-import request from 'supertest'
-import server from '../server'
+import { connectDB } from "../server";
+import db from "../config/db";
 
-describe('GET /api', () => {
-    it('should send back a json response', async () => {
-        const res = await request(server).get('/api')
+jest.mock("../config/db");
 
-        expect(res.status).toBe(200)
-        expect(res.headers['content-type']).toMatch(/json/)
-        expect(res.body.msg).toBe('Desde API')
-    })
-})
+describe("connectDB", () => {
+    it("should handle database connection error", async () => {
+        jest.spyOn(db, "authenticate").mockRejectedValueOnce(
+            new Error("Hubo un error al conectar a la BD")
+        );
+        const consoleSpy = jest.spyOn(console, "log");
+
+        await connectDB();
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+            expect.stringContaining("Hubo un error al conectar a la BD")
+        );
+    });
+});
